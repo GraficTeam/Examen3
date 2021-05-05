d40b51b3182a457a9419def17a8a438ce4ac4386
Brandon <71857025+BRC7380T@users.noreply.github.com>
2021-05-05 16:09:40 -0500

    Add metodo palabras

--- Exam3/src/test.java
+++ Exam3/src/test.java
@@ -2,26 +2,11 @@ import java.util.*;
 import java.text.Collator;
 
 public class test {
-    public static int comparacion(String cad1, String cad2){
-        Collator compara = Collator.getInstance();
-        compara.setStrength(Collator.PRIMARY);
-        if (compara.compare(cad1, cad2) == 0)
-            return 0;
-        else if (compara.compare(cad1, cad2) < 0)
-            return -1;
-        else if (compara.compare(cad1, cad2) > 0)
-            return 1;
-        return 0;
-    }
-    
-    public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-        String frase, frase1, frase2;
-        int n, i, resul;
+    static int n;
 
-        System.out.println("Ingrese texto");
-        frase = teclado.nextLine();
-        StringTokenizer tokens = new StringTokenizer(frase); //cracion de StringTokenizer
+    public static String[] palabras(String cad) {
+        int i;
+        StringTokenizer tokens = new StringTokenizer(cad); //cracion de StringTokenizer
         n = tokens.countTokens();//numero de tokens del string
         String[] aux = new String[n];
         String[] palabras = new String[n];
@@ -34,9 +19,32 @@ public class test {
         //elimina signos de puntuacion y pasa las palabras limpias a otro vector
         for (i = 0; i < n; i++)
             palabras[i] = aux[i].replaceAll("\\p{Punct}", "");
-        //muestra lo elementos del vector en pantalla
+        return palabras;
+    }
+
+    public static int comparacion(String cad1, String cad2) {
+        Collator compara = Collator.getInstance();
+        compara.setStrength(Collator.PRIMARY);
+        if (compara.compare(cad1, cad2) == 0)
+            return 0;
+        else if (compara.compare(cad1, cad2) < 0)
+            return -1;
+        else if (compara.compare(cad1, cad2) > 0)
+            return 1;
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        String frase, frase1, frase2;
+        String[] aux;
+        int resul, i;
+
+        System.out.println("Ingrese texto");
+        frase = teclado.nextLine();
+        aux = palabras(frase);
         for (i = 0; i < n; i++)
-            System.out.println(palabras[i]);
+            System.out.println(aux[i]);
         /*Ejemplo de comparacion de dos palabras
         retorna: 1 si frase1 es mayor a frase2
                 -1 si frase1 es menor a frase2
